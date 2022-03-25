b5a2399c9e0435fc5e319c73d606d7955058d880
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-26 01:25:30 +0600

    handle cart delete button and add fontAwesome icon

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,17 +1,30 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import './Cart.css'
 
-const Cart = ({teams, details}) => {
+const Cart = ({teams, details, setDetails}) => {
+
+  const deleteCart = add => {
+    setDetails(details.filter(item => item.id !== add.id));
+    // console.log(remove.id);
+  }
+
   return (
     <div>
       <h3>Total Employe: {teams.length}</h3>
-      <h5>Selected Employe </h5>
+      <h5>Selected Employe {details.length}</h5>
       {
         details.map(add => {
           return (
             <div className="cart-details d-flex align-items-center" key = {add.id}>
               <img src= {add.img} alt="" />
               <h4 className='px-3'>{add.name}</h4>
+            <div onClick= {() => deleteCart(add)} className="icon">
+              <FontAwesomeIcon 
+                icon = {faTrash}
+                />
+            </div>
             </div>
           )
         })
--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,9 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import './Team.css';
 
 const Team = (props) => {
 
-  const {name, img, id, salary} = props.team;
+  const {name, img, salary} = props.team;
   const {handleClick, team} = props;
 
   return (
@@ -11,8 +13,13 @@ const Team = (props) => {
       <img src= {img} alt="" />
       <h4 className='pt-4'>{name}</h4>
       <p>Salary: {salary}</p>
-      <button onClick= {() => handleClick(team)} className='btn-style position-absolute border-0'>
-        <p className='m-0'>Select Employe</p>
+      <button onClick= {() => handleClick(team)} className='btn-style position-absolute border-0 d-flex justify-content-center align-items-center'>
+        <p className='m-0 px-2'>Select Employe</p>
+
+        <FontAwesomeIcon 
+        icon = {faShoppingCart}
+
+        />
       </button>
     </div>
   );
